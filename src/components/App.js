ed3b648121339ef519105760cb3b628e06a00acb
ale-lit <zeldor@ya.ru>
2022-02-14 10:23:47 +0300

    clone repository

---
+++ src/components/App.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import EditProfilePopup from "./EditProfilePopup";
+import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import {api} from '../utils/api.js';
+
+function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+
+  const [selectedCard, setSelectedCard] = useState({});
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setSelectedCard({});
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  }
+
+  function handleUpdateUser(newUser) {
+    api.editUserInfo(newUser.name, newUser.about)
+      .then((user) => {
+        setCurrentUser(user);
+        closeAllPopups();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleUpdateAvatar(newAvatarUrl) {
+    api.changeAvatar(newAvatarUrl.avatar)
+      .then((user) => {
+        setCurrentUser(user);
+        closeAllPopups();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api.getAllNeededData()
+      .then(([cards, user]) => {
+        setCurrentUser(user);
+        setCards(cards);        
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);  
+
+  function handleCardLike(card) {
+    // Снова проверяем, есть ли уже лайк на этой карточке
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    
+    // Отправляем запрос в API и получаем обновлённые данные карточки
+    api.changeLikeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+          setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } 
+
+  function handleCardDelete(id) {
+    api.deleteCard(id)
+      .then(() => {
+        setCards(cards.filter(card => card._id !== id));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleAddPlaceSubmit(card) {
+    api.postCard(card.name, card.link)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <Header />
+      <Main
+        onCardClick={handleCardClick}
+        onEditAvatar={handleEditAvatarClick}
+        onEditProfile={handleEditProfileClick}
+        onAddPlace={handleAddPlaceClick}
+        cards={cards}
+        onCardLike={handleCardLike}
+        onCardDelete={handleCardDelete}
+      />
+      <Footer />      
+
+      <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} onUpdateAvatar={handleUpdateAvatar} />
+
+      <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
+
+      <AddPlacePopup isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} onAddPlace={handleAddPlaceSubmit} />
+      
+      <PopupWithForm onClose={closeAllPopups} name="submit" title="Вы уверены?">
+        <input type="submit" value="Да" className="popup__save-button" />
+      </PopupWithForm>
+
+      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header root__container">
+      <div className="header__logo"></div>
+    </header>
+  );
+}
+
+export default Header;
